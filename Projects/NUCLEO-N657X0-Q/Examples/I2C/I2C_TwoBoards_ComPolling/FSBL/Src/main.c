17cce0c3994ec2af443bd853568b2eacda2bc16a
Andy Wo <zwo@lenovo.com>
2026-03-25 19:18:54 +0900

    feat(i2c): Add I2C_TwoBoards_ComPolling example for master/slave communication using polling.

--- Projects/NUCLEO-N657X0-Q/Examples/I2C/I2C_TwoBoards_ComPolling/FSBL/Src/main.c
+++ Projects/NUCLEO-N657X0-Q/Examples/I2C/I2C_TwoBoards_ComPolling/FSBL/Src/main.c
@@ -366,7 +366,7 @@ static void MX_I2C1_Init(void)
   hi2c1.Instance = I2C1;
   hi2c1.Init.Timing = 0x00F02781;
   hi2c1.Init.OwnAddress1 = I2C_ADDRESS;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_10BIT;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
   hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
