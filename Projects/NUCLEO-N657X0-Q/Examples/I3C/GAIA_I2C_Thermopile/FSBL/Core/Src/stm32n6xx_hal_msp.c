ba8d6a8c4fb2e5cb39fe2a0f2333b63ccd77e2f6
Andy Wo <zwo@lenovo.com>
2026-04-02 19:54:42 +0900

    feat: add I2C and I3C sensor communication example for NUCLEO-N657X0-Q

--- Projects/NUCLEO-N657X0-Q/Examples/I3C/GAIA_I2C_Thermopile/FSBL/Core/Src/stm32n6xx_hal_msp.c
+++ Projects/NUCLEO-N657X0-Q/Examples/I3C/GAIA_I2C_Thermopile/FSBL/Core/Src/stm32n6xx_hal_msp.c
@@ -311,8 +311,8 @@ void HAL_I3C_MspInit(I3C_HandleTypeDef* hi3c)
     handle_GPDMA1_Channel0.Init.Direction = DMA_MEMORY_TO_PERIPH;
     handle_GPDMA1_Channel0.Init.SrcInc = DMA_SINC_INCREMENTED;
     handle_GPDMA1_Channel0.Init.DestInc = DMA_DINC_FIXED;
-    handle_GPDMA1_Channel0.Init.SrcDataWidth = DMA_SRC_DATAWIDTH_BYTE;
-    handle_GPDMA1_Channel0.Init.DestDataWidth = DMA_DEST_DATAWIDTH_BYTE;
+    handle_GPDMA1_Channel0.Init.SrcDataWidth = DMA_SRC_DATAWIDTH_WORD;
+    handle_GPDMA1_Channel0.Init.DestDataWidth = DMA_DEST_DATAWIDTH_WORD;
     handle_GPDMA1_Channel0.Init.Priority = DMA_LOW_PRIORITY_LOW_WEIGHT;
     handle_GPDMA1_Channel0.Init.SrcBurstLength = 1;
     handle_GPDMA1_Channel0.Init.DestBurstLength = 1;
