b3320cf7dfeb3c27900a585764612248cd4c8ffd
Andy Wo <zwo@lenovo.com>
2026-04-10 19:06:23 +0900

    feat: implement I2C and I3C communication test routines for GAIA thermopile sensor project

--- Projects/NUCLEO-N657X0-Q/Examples/I3C/GAIA_I2C_Thermopile/FSBL/Core/Src/stm32n6xx_it.c
+++ Projects/NUCLEO-N657X0-Q/Examples/I3C/GAIA_I2C_Thermopile/FSBL/Core/Src/stm32n6xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern I2C_HandleTypeDef hi2c3;
 extern I3C_HandleTypeDef hi3c1;
 /* USER CODE BEGIN EV */
 
@@ -212,6 +213,34 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32n6xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles I2C3 event interrupt.
+  */
+void I2C3_EV_IRQHandler(void)
+{
+  /* USER CODE BEGIN I2C3_EV_IRQn 0 */
+
+  /* USER CODE END I2C3_EV_IRQn 0 */
+  HAL_I2C_EV_IRQHandler(&hi2c3);
+  /* USER CODE BEGIN I2C3_EV_IRQn 1 */
+
+  /* USER CODE END I2C3_EV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles I2C3 error interrupt.
+  */
+void I2C3_ER_IRQHandler(void)
+{
+  /* USER CODE BEGIN I2C3_ER_IRQn 0 */
+
+  /* USER CODE END I2C3_ER_IRQn 0 */
+  HAL_I2C_ER_IRQHandler(&hi2c3);
+  /* USER CODE BEGIN I2C3_ER_IRQn 1 */
+
+  /* USER CODE END I2C3_ER_IRQn 1 */
+}
 
 /**
   * @brief This function handles I3C1 event interrupt.
